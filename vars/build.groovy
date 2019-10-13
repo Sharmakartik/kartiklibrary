58ecea87e8db9dc16239376a3c8eca001dba6746
Sharmakartik <Kartik.gs@outlook.com>
2019-10-13 12:02:24 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -5,9 +5,8 @@ import scr.Shell.*
 def call(message='package'){
     def h= new Shell(this)
     //h.mvn'clean'
-    bat '''
-        mvn ${message}
-     '''
+    bat"mvn ${message}"
+ 
     echo "build from script"
     
  }
