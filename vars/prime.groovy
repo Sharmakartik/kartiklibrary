a26fd7f37494e578fd927daaa1ec32a52bfadb2c
Sharmakartik <Kartik.gs@outlook.com>
2019-10-13 19:49:08 +0530

    Update prime.groovy

--- vars/prime.groovy
+++ vars/prime.groovy
@@ -1,6 +1,5 @@
 @Grab('org.apache.commons:commons-math3:3.4.1')
-import org.apache.commons.math3.primes.Prime
-
+import org.apache.commons.math3.primes.Primes
 void parallelize(int count) {
   if (!Primes.isPrime(count)) {
     error "${count} was not prime"
